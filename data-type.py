8bfba63e9f3b1686bb3b037bff4860c2db3df0e2
rakib <jhadul.islam@itconquest.com>
2020-10-25 15:09:12 +0600

    add data type

---
+++ data-type.py
@@ -0,0 +1,35 @@
+# Numeric Data Type
+
+# Integer
+x = 10
+print(type(x))
+
+# Float
+y = 10.10
+print(type(y))
+
+# Complex
+z = 10 + 1j
+print(type(z))
+
+# Boolean
+print(type(x < y))
+
+# Sequence Data Type
+
+# String
+x = "rakib"
+print(type(x))
+
+# List
+x = [1,2,3,4,5]
+print(type(x))
+
+# Set
+x = {1,2,3,4,5}
+print(type(x))
+
+# Tuple
+x = (1,2,3,4,5)
+print(type(x))
+
